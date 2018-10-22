eac0fcef9e3bd2c8d76216ddd208722793f35be8
Michael Wenting <michael.wenting@fabriquartz.com>
2018-10-22 15:02:43 +0200

    Re-add old eslint.rc

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,8 @@ module.exports = {
     "no-console":  ["error", { allow: ["error"] }],
     "quotes":      ["error", "single", { allowTemplateLiterals: true }],
     "brace-style": ["error", "1tbs", { allowSingleLine: true }],
-    "indent":      ["error", 2, { FunctionExpression: { body: 1 } }],
+    "indent":      ["error", 2, { FunctionExpression: { body: 1 },
+                                  CallExpression:     { arguments: "off" } }],
     "key-spacing": ["error", { multiLine: { beforeColon: false },
                                align:     { beforeColon: false, on: "value" } }],
     "max-len":     ["error", { code: 85 }],
@@ -28,33 +29,5 @@ module.exports = {
     "max-statements-per-line": "off",
     "new-cap":                 "off",
     "operator-linebreak":      "off"
-  },
-  overrides: [
-    // node files
-    {
-      files: [
-        'ember-cli-build.js',
-        'index.js',
-        'testem.js',
-        'blueprints/*/index.js',
-        'config/**/*.js',
-        'tests/dummy/config/**/*.js'
-      ],
-      excludedFiles: [
-        'addon/**',
-        'addon-test-support/**',
-        'app/**',
-        'tests/dummy/app/**'
-      ],
-      parserOptions: {
-        sourceType: 'script',
-        ecmaVersion: 2015
-      },
-      env: {
-        browser: false,
-        node: true
-      },
-      plugins: ['node']
-    }
-  ]
+  }
 };
