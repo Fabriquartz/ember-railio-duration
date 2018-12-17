255fb2e9148b99e5e7cf79a5bc4b2ef877c1d2cd
Pieter <Pieter901@hotmail.com>
2018-12-17 10:59:39 +0100

    Updated ember to version 3.6 (#10)
    * Updated ember to version 3.6
    
    * Updated Node version for travis
    
    * Added package for linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,5 +29,38 @@ module.exports = {
     "max-statements-per-line": "off",
     "new-cap":                 "off",
     "operator-linebreak":      "off"
-  }
+  },
+  overrides: [
+    // node files
+    {
+      files: [
+        '.eslintrc.js',
+        '.template-lintrc.js',
+        'ember-cli-build.js',
+        'index.js',
+        'testem.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'tests/dummy/config/**/*.js'
+      ],
+      excludedFiles: [
+        'addon/**',
+        'addon-test-support/**',
+        'app/**',
+        'tests/dummy/app/**'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      },
+      plugins: ['node'],
+      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+        // add your custom rules and overrides for node files here
+      })
+    }
+  ]
 };
