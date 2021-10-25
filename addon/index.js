8bd0f75553cfb4ab54bbed1be98609fc631606ba
Pieter van Laatum <pieter.van.laatum@fabriquartz.com>
2021-10-25 14:48:29 +0200

    Fix linting

--- addon/index.js
+++ addon/index.js
@@ -1,14 +1,18 @@
 import { computed } from '@ember/object';
 import convertUnit from 'ember-railio-convert-unit';
 
-function add(a, b)        { return a.valueOf() + b.valueOf(); }
-function difference(a, b) { return b.valueOf() - a.valueOf(); }
+function add(a, b) {
+  return a.valueOf() + b.valueOf();
+}
+function difference(a, b) {
+  return b.valueOf() - a.valueOf();
+}
 
-export default function(startPropName, endPropName) {
+export default function (startPropName, endPropName) {
   return computed(startPropName, endPropName, {
     get() {
       let start = this.get(startPropName);
-      let end   = this.get(endPropName);
+      let end = this.get(endPropName);
 
       if (start instanceof Date && end instanceof Date) {
         return convertUnit(difference(start, end), 'milliseconds', 'hours');
@@ -22,13 +26,13 @@ export default function(startPropName, endPropName) {
 
       if (start instanceof Date) {
         let milliseconds = convertUnit(value, 'hours', 'milliseconds');
-        let end          = new Date(add(start, milliseconds));
+        let end = new Date(add(start, milliseconds));
 
         this.set(endPropName, end);
 
         return convertUnit(difference(start, end), 'milliseconds', 'hours');
       }
       return null;
-    }
+    },
   });
 }
--- tests/unit/duration-property-test.js
+++ tests/unit/duration-property-test.js
@@ -4,27 +4,28 @@ import { module, test } from 'qunit';
 
 import durationProperty from 'ember-railio-duration';
 
+// eslint-disable-next-line ember/no-classic-classes
 const DurationModel = EmberObject.extend({
   start: null,
-  end:   null,
+  end: null,
 
-  duration: durationProperty('start', 'end')
+  duration: durationProperty('start', 'end'),
 });
 
-module('Unit | Computed Duration Property', function() {
-  test('Duration is computed', function(assert) {
+module('Unit | Computed Duration Property', function () {
+  test('Duration is computed', function (assert) {
     let object = DurationModel.create({
       start: new Date(2000, 1, 1),
-      end:   new Date(2000, 1, 1, 12, 15)
+      end: new Date(2000, 1, 1, 12, 15),
     });
 
     assert.equal(object.get('duration'), 12.25);
   });
 
-  test('Setting duration computes end time', function(assert) {
+  test('Setting duration computes end time', function (assert) {
     let object = DurationModel.create({
       start: new Date(2000, 1, 1),
-      end:   new Date(2000, 1, 1, 12)
+      end: new Date(2000, 1, 1, 12),
     });
 
     object.set('duration', 20);
